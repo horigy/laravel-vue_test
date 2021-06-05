a9af54b80300d2bb4ba7da7c1015f00fc3e2bad5
Nikolay Gerasimov <fox-race@yandex.ru>
2021-06-05 15:43:12 +0300

    first commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <h2>Кто вы?</h2>
+        <div class="new-user">
+            <label>Имя</label>
+            <input v-model="newUser.name" type="text">
+            <label>Фамилия</label>
+            <input v-model="newUser.surname" type="text">
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            newUser: {
+                type: Object,
+                requred: true
+            }
+        },
+        methods: {
+            refreshUser() {
+                this.$emit('newUser', this.newUser)
+            }
+        }
+    }
+</script>
---
+++ resources/js/components/TestComponent.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="container">
+        <h1>Hello, Laravel!</h1>
+        <h2>Меня зовут</h2>
+        <div class="user">
+            <div class="user-data">
+                <span>{{user.name}}</span>
+                <span>{{user.surname}}</span>
+            </div>
+            <div class="user-pic">
+                <img v-bind:src="user.imgUrl" alt="">
+            </div>
+        </div>
+
+
+
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            user: {
+                name: 'Nikolay',
+                surname: 'Gerasimov',
+                imgUrl: 'https://media.licdn.cn/dms/image/C5103AQE-2B69yAgJ4w/profile-displayphoto-shrink_400_400/0/1517550089699?e=1628121600&v=beta&t=hs9Fwa9bpwjdCbW8w1fFdAQEusAHi_qYFq6syyzmcBY'
+            },
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    margin: 10% 0;
+}
+.user {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-size: 60px;
+}
+.user-pic > img {
+    border-radius: 50%;
+}
+</style>
